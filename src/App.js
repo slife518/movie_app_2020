e4c6def2a8207583b182609de4176d35aca65d6b
jk <slife705@naver.com>
2020-11-16 08:35:46 +0900

    route completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
-import Home from "./routes/Home";
+// import Home from "./routes/Home";
+import {HashRouter, Route} from 'react-router-dom';
 import "./App.css";
+import About from './routes/About';
+import Home from './routes/Home'
+
 function App() {
-  return <Home />;
+  return (
+   <HashRouter>
+     <Route path="/" exact={true} component={Home}/>
+     <Route path="/about"  component={About}/>
+     {/* <Route path="/home">
+       <h1> Home</h1>
+     </Route>
+     <Route path="/home/introduction">
+       <h1>introduction</h1>
+     </Route>
+     <Route path="/about">
+       <h1>About</h1>
+     </Route> */}
+   </HashRouter>
+  );
 }
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./About.css";
+
+function About(){
+    return (
+        <div className="about_container">
+            <span>About this page: I built it beacuse I love movies.</span>;
+            <span> - George Orwell, 1984</span>
+        </div>
+    ) 
+}
+
+export default About;
