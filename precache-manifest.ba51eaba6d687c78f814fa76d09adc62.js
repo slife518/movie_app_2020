5286b121133fcb77c24b9f2fbed2ab2502a048bd
jk <slife705@naver.com>
2020-11-19 08:23:51 +0900

    Updates

---
+++ precache-manifest.ba51eaba6d687c78f814fa76d09adc62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2c4631853f5e7674ef02fbb93b8e2a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "fcdcb3fe9ec2359bb25e",
+    "url": "/movie_app_2020/static/css/main.4462d18d.chunk.css"
+  },
+  {
+    "revision": "3dc07eb8fe714f672825",
+    "url": "/movie_app_2020/static/js/2.7ab01957.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.7ab01957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcdcb3fe9ec2359bb25e",
+    "url": "/movie_app_2020/static/js/main.b0874ab1.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
